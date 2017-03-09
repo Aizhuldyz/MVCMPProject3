cc90761bd3590607c6272d519d378911805a073c
Aizhuldyz_Gabbassova <Aizhuldyz_Gabbassova@EPKZASTW0008>
2017-03-09 20:04:19 +0600

    Model View Controllers - basic

---
+++ MVCApp/Repository/BadgeRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MVCApp.Models;
+
+namespace MVCApp.Repository
+{
+    public class BadgeRepository
+    {
+        public ApplicationDbContext Context;
+
+        public BadgeRepository()
+        {
+            Context = new ApplicationDbContext();
+        }
+
+        public List<Badge> GetAll()
+        {
+            return Context.Badges.ToList();
+        }
+    }
+}
---
+++ MVCApp/Repository/PersonRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MVCApp.Models;
+
+namespace MVCApp.Repository
+{
+    public class PersonRepository
+    {
+        public ApplicationDbContext Context;
+
+        public PersonRepository()
+        {
+            Context = new ApplicationDbContext();
+        }
+
+        public List<Person> GetAll()
+        {
+            return Context.Persons.ToList();
+        }
+
+    }
+}
