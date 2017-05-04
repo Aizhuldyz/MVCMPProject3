5f03af19f186f71545ca10142db9144fe918b618
Aizhuldyz_Gabbassova <Aizhuldyz_Gabbassova@EPKZASTW0008>
2017-05-04 19:39:26 +0600

    Signin after  adding to users role after registration

--- MVCApp/Controllers/AccountController.cs
+++ MVCApp/Controllers/AccountController.cs
@@ -155,7 +155,7 @@ namespace MVCApp.Controllers
                 var result = await UserManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
                 {
-                    await SignInManager.SignInAsync(user, isPersistent:false, rememberBrowser:false);
+                    
 
                     // For more information on how to enable account confirmation and password reset please visit http://go.microsoft.com/fwlink/?LinkID=320771
                     // Send an email with this link
@@ -163,7 +163,7 @@ namespace MVCApp.Controllers
                     // var callbackUrl = Url.Action("ConfirmEmail", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);
                     // await UserManager.SendEmailAsync(user.Id, "Confirm your account", "Please confirm your account by clicking <a href=\"" + callbackUrl + "\">here</a>");
                     await this.UserManager.AddToRoleAsync(user.Id, "Users");
-
+                    await SignInManager.SignInAsync(user, isPersistent: false, rememberBrowser: false);
                     return RedirectToAction("Index", "Home");
                 }
                 AddErrors(result);
